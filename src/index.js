f832ae52ddf6ba4778c77d7780134c4715e33a1e
Mijael Flores Vega <68754287+MijaelFV@users.noreply.github.com>
2021-09-30 02:22:29 -0300

    Added PWA Support
    #116

--- src/index.js
+++ src/index.js
@@ -2,8 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { DadApp } from './DadApp';
 import './styles/index.scss'
+import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <DadApp />,
   document.getElementById('root')
 );
+
+serviceWorker.register();
